9445b018ae9f39c12a939110ef04c5fbe390c828
Snlg <429759802@qq.com>
2016-11-10 01:49:27 +0800

    addpower

--- game.js
+++ game.js
@@ -27,7 +27,7 @@
         }
     }
     function horseF(x,y,j){
-        console.log(x,y,oldPosX,oldPosY)
+        //console.log(x,y,oldPosX,oldPosY);
         //alert(x+'和'+y)
         if((x == oldPosX+2 && y==oldPosY-1)||(x == oldPosX+2 && y==oldPosY+1)||(x == oldPosX-2 && y==oldPosY-1)||(x == oldPosX-2 && y==oldPosY+1)||(x == oldPosX-1 && y==oldPosY+2)||(x == oldPosX+1 && y==oldPosY+2)||(x == oldPosX-1 && y==oldPosY-2)||(x == oldPosX+1 && y==oldPosY-2)){
             liObj[j].appendChild(horseObj[0]);
@@ -35,7 +35,8 @@
     }
 
     var map = {
-        init : function(){//入口
+        //入口
+        init : function(){
             var self=this;
             self.bindEvt();
         },
@@ -56,9 +57,44 @@
                 var listObj = liObj[midb].getElementsByClassName('list')[0];
                 listObj.setAttribute("style","height:50%;top:50%");
             }
-            liObj[48].innerHTML +="<div class='horse'></div>"
-            horseObj[0].innerHTML = '馬';
+            //创建四周
+            for(var sleft=0;sleft<9;sleft++){
+                liObj[sleft].getElementsByClassName('list')[0].setAttribute("style","height:50%;top:50%");
+                liObj[89-sleft].getElementsByClassName('list')[0].setAttribute("style","height:50%;");
+                liObj[sleft*9].getElementsByClassName('line')[0].setAttribute("style","width:50%;left:50%");
+                liObj[(sleft+1)*9-1].getElementsByClassName('line')[0].setAttribute("style","width:50%;");
+                liObj[liObj.length-1].getElementsByClassName('line')[0].setAttribute("style","width:50%;");
+                liObj[liObj.length-9].getElementsByClassName('line')[0].setAttribute("style","width:50%;left:50%");
+            }
+            self.starGame();
             self.getPos();
+        },
+        //布置棋子初始位置 并且作为游戏的开始
+        starGame : function(){
+            var topPos= [[0,'car'],[1,'horse'],[2,'elephant'],[3,'scholar'],[4,'king'],[5,'scholar'],[6,'elephant'],[7,'horse'],[8,'car'],[19,'fire'],[25,'fire'],[27,'soldier'],[29,'soldier'],[31,'soldier'],[33,'soldier'],[35,'soldier']];
+            function addPos(pos,name,paly){
+                liObj[pos].innerHTML +="<div class="+"'"+name+" "+paly+"'"+"></div>";
+            }
+            //console.log(topPos[1][0],topPos[1][1]);
+            for(var posnum=0;posnum<topPos.length;posnum++){
+                addPos(topPos[posnum][0],topPos[posnum][1],'playR');
+                addPos(89-(topPos[posnum][0]),topPos[posnum][1],'playB');
+            }
+            //给各个棋子添加内容
+            function addtext(ele,what){
+                var eleObj = boxObj.getElementsByClassName(ele);
+                //console.log(eleObj);
+                for (var kk=0; kk<eleObj.length;kk++){
+                    eleObj[kk].innerHTML = what;
+                }
+            };
+            addtext('horse',"馬");
+            addtext('car',"车");
+            addtext('elephant',"象");
+            addtext('soldier',"兵");
+            addtext('fire',"炮");
+            addtext('king',"将");
+            addtext('scholar',"士");
         },
         //点击后获得对应位置
         getPos : function(){
@@ -70,13 +106,13 @@
                         //化成坐标;
                         var x = j%9;
                         var y = parseInt(j/9);
-
+                        console.log(j)
                         //判断当前点击处若没有被选中则选中并且添加闪烁
                         var ziObj = liObj[j].getElementsByTagName('div');
                         if(ziObj.length>2){
                             //子元素div个数大于2说明该位置有棋子
                             if(!liObj[j].getAttribute('data-status')){
-                                console.log(chessPieces)
+                                //console.log(chessPieces);
                                 chessPieces.setAttribute('data-status',"choose");
                                 addClass(chessPieces,'flash');
                                 //保存原来位置的XY
